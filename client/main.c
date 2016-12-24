df4f874984d75581460ff1e482a6df444c69023f
M3tiii <m1995g@gmail.com>
2016-12-24 03:57:12 +0100

    Watched with fork.

--- client/main.c
+++ client/main.c
@@ -16,6 +16,16 @@ typedef int Int32;
 typedef unsigned char UInt8;
 typedef unsigned int UInt32;
 
+void readHeader(int socket, unsigned int x, void* length) {
+    int result;
+    result = read(socket, length,  x);
+    printf("read: %d\n", length);
+    if (result < 1 )
+    {
+        printf("ERROR #2");
+    }
+}
+
 void readXBytes(int socket, unsigned int x, void* buffer) {
     int bytesRead = 0;
     int result;
@@ -31,33 +41,6 @@ void readXBytes(int socket, unsigned int x, void* buffer) {
     }
 }
 
-void readHeader(int socket, unsigned int x, void* length) {
-    int result;
-    result = read(socket, length,  x);
-    if (result < 1 )
-    {
-        printf("ERROR #2");
-    }
-}
-
-int sendHeader(int socket, unsigned int x) {
-    UInt32 conv = htonl(x);
-    char *data = (char*)&conv;
-    int size = sizeof(conv);
-    int result;
-
-    result = write(socket, data, size);
-    if (result < 0) {
-        if ((errno == EAGAIN) || (errno == EWOULDBLOCK)) {
-            // use select() or epoll() to wait for the socket to be writable again
-        }
-        else if (errno != EINTR) {
-            return -1;
-        }
-    }
-    return 0;
-}
-
 int readMsg (int socket, char** result) {
     UInt32 length = 0;
     readHeader(socket, sizeof(length), (void*)(&length));
@@ -70,6 +53,24 @@ int readMsg (int socket, char** result) {
     return 0;
 }
 
+int sendHeader(int socket, unsigned int x) {
+    UInt32 conv = htonl(x);
+    char *data = (char*)&conv;
+    int size = sizeof(conv);
+    int result;
+    
+    result = write(socket, data, size);
+    if (result < 0) {
+        if ((errno == EAGAIN) || (errno == EWOULDBLOCK)) {
+            // use select() or epoll() to wait for the socket to be writable again
+        }
+        else if (errno != EINTR) {
+            return -1;
+        }
+    }
+    return 0;
+}
+
 void sendMsg (int socket, char* message, UInt32 length) {
     UInt32 sendLength = length;
     sendHeader(socket, sendLength);
@@ -78,30 +79,33 @@ void sendMsg (int socket, char* message, UInt32 length) {
 
 int main(int argc, char *argv[]) {
     char* hostIp = "0.0.0.0";
-    char* hostPort = "3014";
+    char* hostPort = "3011";
     struct hostent* host;
     host = gethostbyname(hostIp);
     int fd = socket(PF_INET, SOCK_STREAM, 0);
     struct sockaddr_in saddr;
     saddr.sin_family = PF_INET;
     saddr.sin_port = htons(atoi(hostPort));
-
+    
     memcpy(&saddr.sin_addr.s_addr, host->h_addr, host->h_length);
     connect(fd, (struct sockaddr*) &saddr, sizeof(saddr));
-
+    
     if(fork() == 0) {
-        //write
-        char* sendBuffer = "HelloServer";
-        sendMsg(fd, sendBuffer, 11);
-
-        //read
-        char* readBuffer;
-        readMsg(fd, &readBuffer);
-        printf("client: %s\n",readBuffer);
-
-        //write
-        sendBuffer = "Hello";
-        sendMsg(fd, sendBuffer, 5);
+        while(1) {
+            char* readBuffer;
+            readMsg(fd, &readBuffer);
+            printf("client: %s\n",readBuffer);
+        }
     }
+    if(fork() == 0) {
+        while(1) {
+            char string [256];
+            printf("Insert: ");
+            gets (string);
+            sendMsg(fd, string, sizeof(string));
+        }
+    }
+    while(1){}
+    
     return 0;
 }
